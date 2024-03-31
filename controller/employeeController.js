2fc0717eacd382e17fbbc0d66a64aa466953a0e3
Juan C Martinez <stc.classcontent@gmail.com>
2024-03-31 18:58:45 -0500

    Create function working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,3 +1,23 @@
+import Employee from "../model/employeeModel.js"
+
+export const create = async(req, res) => {
+    try {
+        const employeeData = new Employee(req.body)
+        const {userName} = employeeData
+        const employeeExist = await Employee.findOne({userName})
+
+        if(employeeExist){
+            return res.status(200).json({message: "Employee already Exists."})
+        }
+
+        const savedEmployee = await employeeData.save();
+        res.status(200).json(savedEmployee)
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error."})
+    }
+}
+
+
 export const fetch = async(red, res) => {
     try {
         res.json("Hello World")
